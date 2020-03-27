463d801b794b4ac6f0c5a235d46a477ae4ae88bd
lc5102 <1015355299@qq.com>
2020-03-27 17:13:01 +0800

    1

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,6 +1,5 @@
-module.exports={
-  outputDir:__dirname+'/../server/admin',
-  publicPath:process.env.NODE_ENV==='production'
-  ? '/admin/'
-  :'/'
+module.exports = {
+  outputDir: __dirname + '/../server/admin',
+  publicPath: process.env.NODE_ENV === 'production' ?
+    '/admin' : '/'
 }
