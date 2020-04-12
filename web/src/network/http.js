eb0d50fa2b095c83943452caab2f7cc4cb3732dd
lc5102 <1015355299@qq.com>
2020-04-12 23:52:09 +0800

    删除文件 web

--- web/src/network/http.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const http = axios.create({
-  baseURL: process.env.VUE_APP_API_URL || '/web/api'
-  // baseURL: 'http://localhost:3000/admin/api'
-})
-
-
-export default http
